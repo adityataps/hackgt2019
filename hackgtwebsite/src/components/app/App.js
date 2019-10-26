130aff29caaf5ece2ebd9d3e24c2ebe39bb28cd3
Aditya Tapshalkar <aditya.taps@gatech.edu>
2019-10-26 07:16:19 -0400

    file input

--- hackgtwebsite/src/components/app/App.js
+++ hackgtwebsite/src/components/app/App.js
@@ -1,48 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../../assets/images/logo.svg';
 import { createWorker } from 'tesseract.js';
 import './App.css';
 
+
 export let ctx;
-alert(ctx.back); //list of the sliced img
+// alert(ctx.back); //list of the sliced img
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// fileSelectedHandler = event => {
+//     console.log(event);
+// }
+
+class App extends Component {
+    fileSelectedHandler = event => {
+        console.log(event);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        {/*Edit <code>src/App.js</code> and save to reload.*/}
+
+                        yuh igor
+
+
+                    </p>
+
+
+                    <div className="FileUpload">
+
+                        <input type="file" onChange = {this.fileSelectedHandler}/>
+
+                    </div>
+
+
+                </header>
+            </div>
+        );
+    }
+
+    // function
+
+    runOcr() {
+        const worker = createWorker();
+        (async () => {
+            await worker.load();
+            await worker.loadLanguage('eng');
+            await worker.initialize('eng');
+            for (const [img1] of ctx.entries()) {
+                const {data: {text}} = await worker.recognize(img1);
+                console.log(text);
+                await worker.terminate();
+            }
+            //const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+            //console.log(text);
+            //await worker.terminate();
+        })();
+    }
 }
 
-function runOcr() {
-    const worker = createWorker();
-    (async () => {
-        await worker.load();
-        await worker.loadLanguage('eng');
-        await worker.initialize('eng');
-        for (const [img1] of ctx.entries()) {
-            const { data: { text } } = await worker.recognize(img1);
-            console.log(text);
-            await worker.terminate();
-        }
-        //const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
-        //console.log(text);
-        //await worker.terminate();
-    })();
-}
 
 export default App;
